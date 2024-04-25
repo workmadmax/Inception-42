fbeb3dc161a65071c02b52b49b2ada726819249d
madmax42 <work.madmax@gmail.com>
2024-04-25 13:45:55 -0300

    mariadb funcionando ok ... consegui salvar o volume

---
+++ srcs/requirements/mariadb/conf/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Start MariaDB
+
+service mariadb start && \
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE}; \
+CREATE USER IF NOT EXISTS ${SQL_USER}@'localhost' IDENTIFIED BY '${SQL_PASSWORD}'; \
+GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO ${SQL_USER}@'%' IDENTIFIED BY '${SQL_PASSWORD}'; \
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}'; \
+FLUSH PRIVILEGES;" && \
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
