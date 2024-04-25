226cd5b8754e4ca4589d6c8dd77937af89b90415
madmax42 <work.madmax@gmail.com>
2024-04-25 18:06:39 -0300

    tive que colocar um sleep para parar de restarta falta testar a persistencia do volume

--- srcs/requirements/mariadb/conf/setup.sh
+++ srcs/requirements/mariadb/conf/setup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+
 # Start MariaDB
 
-service mariadb start && \
+service mariadb start
+
+# wait time for db 9s
+sleep 9
+
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE}; \
 CREATE USER IF NOT EXISTS ${SQL_USER}@'localhost' IDENTIFIED BY '${SQL_PASSWORD}'; \
 GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO ${SQL_USER}@'%' IDENTIFIED BY '${SQL_PASSWORD}'; \
